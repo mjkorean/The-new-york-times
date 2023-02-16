8589f82d5c056af0a471667b672f1b353f1b75e3
전명훈 (Myunghoon Jun) <118811830+mjkorean@users.noreply.github.com>
2023-02-16 14:43:54 +0900

    2nd commit

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ const openSearchBox = () => {
 };
 
 let news = [];
+let page = 1;
+let total_pages = 0;
+
 let menus = document.querySelectorAll(".menus button");
 
 menus.forEach((menu) =>
@@ -34,6 +37,9 @@ const getNews = async () => {
       "x-api-key": "P0oEk0aswua68ewBiwWmv4lioMwirgD5Cq7yKvBJQ7g",
     });
 
+    url.searchParams.set("page", page);
+    console.log("url은?", url);
+
     let response = await fetch(url, { headers: header });
     let data = await response.json();
 
@@ -41,9 +47,13 @@ const getNews = async () => {
       if (data.total_hits == 0) {
         throw new Error("검색된 결과값이 없습니다.");
       }
+      console.log("받는 데이터가 뭐지?", data);
       news = data.articles;
+      total_pages = data.total_pages;
+      page = data.page;
       console.log(news);
       render();
+      pagination();
     } else {
       throw new Error(data.message);
     }
@@ -116,6 +126,56 @@ const errorRender = (message) => {
   ${message}
 </div>`;
   document.getElementById("news-board").innerHTML = errorHTML;
+};
+
+const pagination = () => {
+  let paginationHTML = ``;
+  // total_page
+  // 내가 보고 있는 현재 page
+  // page group
+  let pageGroup = Math.ceil(page / 5);
+  // last page
+  let last = pageGroup * 5;
+  // first page
+  let first = last - 4;
+  // first~last 페이지 프린트
+
+  // total page < 5일 경우, 남은 페이지 수 만큼만 출력 - last, first
+  // << >> 버튼 만들기 - 맨처음, 맨끝으로 가는 버튼
+  // 내가 맨 처음 그룹일때 << 버튼 없음
+  // 내가 맨 마지막 그룹일때 >> 버튼 없음
+
+  paginationHTML = `<li class="page-item">
+  <a class="page-link" href="#" aria-label="Previous" onclick="moveToPage(${
+    page - 1
+  })">
+    <span aria-hidden="true">&lt;</span>
+  </a>
+</li>`;
+
+  for (let i = first; i <= last; i++) {
+    paginationHTML += `<li class="page-item ${
+      page == i ? "active" : ""
+    }"><a class="page-link" href="#" onclick="moveToPage(${i})">${i}</a></li>`;
+  }
+
+  paginationHTML += `<li class="page-item">
+  <a class="page-link" href="#" aria-label="Next" onclick="moveToPage(${
+    page + 1
+  })>
+    <span aria-hidden="true">&gt;</span>
+  </a>
+</li>`;
+
+  document.querySelector("pagination").innerHTML = paginationHTML;
+};
+
+const moveToPage = (pageNum) => {
+  // 이동하고싶은 페이지 알기
+  page = pageNum;
+
+  // 이동하고싶은 페이지 가지고 api 다시 호출
+  getNews();
 };
 
 searchButton.addEventListener("click", getNewsByKeyword);
